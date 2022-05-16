d0ce76d720b104cb093cda01c5fb2168bd83cf9a
ecourse <smkang@codenuri.co.kr>
2022-05-16 13:58:16 +0900

    Add files via upload

---
+++ DAY1/5_도형편집기1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+// 1. 모든 도형을 타입으로 설계하면 편리하다.
+
+class Rect
+{
+public:
+	void draw() { std::cout << "draw rect" << std::endl; }
+};
+
+
+int main()
+{
+
+}
+
