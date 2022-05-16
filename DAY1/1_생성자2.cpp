31d5c988e16354f1920bb2c9d9c5f0b24a0781f1
ecourse <smkang@codenuri.co.kr>
2022-05-16 09:34:28 +0900

    Add files via upload

---
+++ DAY1/1_생성자2.cpp
@@ -0,0 +1,32 @@
+// 1_생성자2-1.cpp
+#include <string>
+#include <iostream>
+
+class People
+{
+	std::string name;
+	int age;
+public:
+	// 모든 객체는 항상 유효한 상태로 만들어져야 합니다
+	People(const std::string& name, int age) : name(name), age(age) {}
+};
+
+class Student : public People
+{
+	int id;
+public:
+//	Student(int id) : id(id) {} // Student(int id) : People(), id(id) {}
+
+	Student(const std::string& name, int age, int id)
+		: People(name, age), id(id) {}
+};
+
+int main()
+{
+//	People p1;	// 이렇게 객체를 만들수 있다는 것이 좋을까요 ? (초기화 되지 않은 객체)
+	People p2("kim", 20);
+
+	Stduent s(99);
+}
+
+
