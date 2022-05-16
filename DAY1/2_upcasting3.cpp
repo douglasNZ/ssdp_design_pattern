e848e1650734ff476563ab53744fd8702d6e4295
ecourse <smkang@codenuri.co.kr>
2022-05-16 10:40:50 +0900

    Add files via upload

--- DAY1/2_upcasting3.cpp
+++ DAY1/2_upcasting3.cpp
@@ -1,7 +1,12 @@
+#include <iostream>
+
+
 // upcasting3.cpp
 class Animal 
 {
 public: 
+	virtual ~Animal() {}	// 모든 기반 클래스의 소멸자는 반드시 
+							// 가상함수가 되어야 합니다.
 	int age;
 };
 class Cat : public Animal 
@@ -28,9 +33,21 @@ void NewYear(Animal* p)   // 모든 동물을 인자로 받는 함수.
 //	p->color = 10; // error. p의 타입이 Animal* 이므로 Animal 멤버가접근가능
 
 	// Dog 고유 멤버에 접근하려면 캐스팅 필요
-	Dog* pDog = static_cast<Dog*>(p);
+	// down cast : 기반 클래스 포인터를 파생 클래스 포인터로 다시 캐스팅 하는것
+	// static_cast  : 실제 객체가 Dog가 아니라도 항상 성공. 위험하다.
+	// dynamic_cast : 실제 객체가 Dog 인 경우만 성공, 아니면 0 반환 
+	//				단, 반드시 가상함수가 한개 이상 있는 경우만 사용가능합니다.
+//	Dog* pDog = static_cast<Dog*>(p);
+
+	Dog* pDog = dynamic_cast<Dog*>(p);
 
 	std::cout << pDog << std::endl;
+
+	if (pDog != nullptr)
+	{
+		pDog->color = 10;
+	}
+
 }
 
 int main()
