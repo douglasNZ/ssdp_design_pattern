1e19408801b6f86b9c5c780fefe3e2abd94dbda8
ecourse <smkang@codenuri.co.kr>
2022-05-16 09:21:08 +0900

    Add files via upload

---
+++ DAY1/1_생성자1.cpp
@@ -0,0 +1,38 @@
+//생성자1.cpp - 3page
+#include <iostream>
+
+// 생성자 호출의 정확한 원리 
+
+class Base
+{
+public:
+	Base() { std::cout << "Base()" << std::endl; }
+	Base(int a){ std::cout << "Base(int)" << std::endl; }
+	~Base()    { std::cout << "~Base()" << std::endl; }
+};
+class Derived : public Base
+{
+public:		
+	// 사용자가 만든 코드		// 컴파일러가 변경한 코드
+	Derived()				// Derived() : Base()
+	{ 
+		std::cout << "Derived()" << std::endl; 
+	}
+	Derived(int a)			// Derived(int a) : Base()
+	{ 
+		std::cout << "Derived(int)" << std::endl; 
+	}
+	~Derived()		
+	{ 
+		std::cout << "~Derived()" << std::endl; 
+
+		// ~Base::Base() 
+	}
+};
+int main()
+{
+//	Derived d1;   // call Derived::Derived()
+	Derived d2(5);// call Derived::Derived(int)
+
+}
+
